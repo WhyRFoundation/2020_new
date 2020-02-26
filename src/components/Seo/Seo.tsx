a9fe58d0b2a974e839d1c2289b9cfc53b190892c
ebuszylo <emil.buszylo@gmail.com>
2020-02-26 21:54:10 +0100

    chore: link change

--- src/components/Seo/Seo.tsx
+++ src/components/Seo/Seo.tsx
@@ -72,10 +72,6 @@ export const Seo: React.FC<SeoProps> = ({
       link={[{ rel: `canonical`, href: metaCanonical }]}
       meta={[
         {
-          httpEquiv: 'refresh',
-          content: '0; URL=http://whyr.pl/2020_new/',
-        },
-        {
           name: `description`,
           content: metaDescription,
         },
