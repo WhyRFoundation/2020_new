bd4ab299f47754e9cae462a8cf68bcff176ba0e2
Marcin <m.p.kosinski@gmail.com>
2020-11-24 14:42:23 +0100

    change gatsby plugin for google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -134,7 +134,7 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      resolve: `gatsby-plugin-google-fonts.`,
       options: {
         fonts: [
           {
